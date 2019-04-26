f33934f3979ae4e80b49e44b70dc8e44fd28abec
Hegin Roy C <heginroyc@gmail.com>
2019-04-26 14:49:01 +0530

    Update app.js

--- src/container/App/app.js
+++ src/container/App/app.js
@@ -3,7 +3,8 @@ import React, { Component } from 'react';
 import Header from '../../components/header/index';
 import Footer from '../../components/footer/index';
 import LandingPage from '../LandingPage/index';
-import RegisterForm from '../RegisterForm/index';
+import Menu from '../Menu/index';
+import Players from '../../components/players/index';
 //import Home from '../components/Home/Home';
 import { Route } from 'react-router-dom';
 import history from '../../history.js';
@@ -13,9 +14,9 @@ class App extends Component {
     return (
       <div className="App">
         <Header />
-          <Route exact path="/" component={LandingPage} />
-          <Route path="/form" component={RegisterForm} />
-       <Footer />
+        <Route exact path="/" component={LandingPage} />
+        <Route path="/play" component={Players} />
+        <Footer />
        </div>
 
     );
