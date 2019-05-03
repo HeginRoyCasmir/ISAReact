7e5c48a4c63051f105efada3df0e64dd85f316d5
Hegin Roy C <heginroyc@gmail.com>
2019-05-03 12:37:50 +0530

    Update index.js

--- src/components/players/index.js
+++ src/components/players/index.js
@@ -1,26 +1,28 @@
 import React, { Component } from 'react';
 import './style.css';
-import { Col } from 'react-bootstrap';
-import jobin from '../../images/jobinpp.JPG';
-import Card from "react-bootstrap/Card";
+import { Col, Card } from 'react-bootstrap';
+import jobin from '../../images/jobinbanr.JPG';
+
 
 class Players extends Component {
   render() {
     return (
       <div className="players">
-        <Col xs={12}>
+        <Col xs={12} md={12}>
           <h3 className="athelete">ISA ATHLETES</h3>
           <hr />
         </Col>
-        <Col xs={12}>
-          <div className="basketText">BASKETBALL</div>
-          <Col>
-            <Card style={{ width: '18rem' }}>
-              <Card.Img variant="top" src={jobin} />
+        <Col xs={12} md={12}>
+          <div className="basketText">BASKETBALL</div><br/>
+          <Col md={3}>
+            <Card >
+              <Card.Img variant="top" src={jobin} className="cardImg"/>
               <Card.Body>
-                <Card.Title>Players Details</Card.Title>
+                <Card.Title>Jobin</Card.Title>
                 <Card.Text>
-
+                  <p><span>Height:</span><span>6.1</span></p>
+                  <p><span>Weight:</span><span>64</span></p>
+                  <p><span>Position:</span><span>Ball Handler</span></p>
                 </Card.Text>
                 </Card.Body>
             </Card>
