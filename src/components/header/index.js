0c941c0706c3c13c167e0156fdc5da5dcde2f109
Hegin Roy C <heginroyc@gmail.com>
2019-05-03 12:35:57 +0530

    Update index.js

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -11,27 +11,17 @@ class Header extends Component {
             <h2 className="textLogo">ISA</h2>
           </Navbar.Brand>
           <Col xs={7} md={7} className="menu">
-              <Nav.Link onClick={this.props.onHome} > Home </Nav.Link>
-              <Nav.Link onClick={this.props.onGallery} > Gallery </Nav.Link>
-              <Nav.Link onClick={this.props.onFixtures} > Fixtures </Nav.Link>
-              <Nav.Link onClick={this.props.onJersey} > Jersey </Nav.Link>
-              <Nav.Link onClick={this.props.onPlay} > Players </Nav.Link>
-              <Nav.Link onClick={this.props.onLive} > Live </Nav.Link>
-              <Nav.Link onClick={this.props.onContact} > Contact </Nav.Link>
-           
-
-
-            {/* <NavItem>
-              <Navbar.Link onClick={this.props.onHome} > Home </Navbar.Link>
-              <Navbar.Link onClick={this.props.onGallery} > Gallery </Navbar.Link>
-              <Navbar.Link onClick={this.props.onFixtures} > Fixtures </Navbar.Link>
-              <Navbar.Link onClick={this.props.onJersey} > Jersey </Navbar.Link>
-              <Navbar.Link onClick={this.props.onPlay} > Players </Navbar.Link>
-              <Navbar.Link onClick={this.props.onLive} > Live </Navbar.Link>
-              <Navbar.Link onClick={this.props.onContact} > Contact </Navbar.Link>
-            </NavItem> */}
+            <Nav.Link onClick={this.props.onHome} > Home </Nav.Link>
+            <Nav.Link onClick={this.props.onGallery} > Gallery </Nav.Link>
+            <Nav.Link onClick={this.props.onFixtures} > Fixtures </Nav.Link>
+            <Nav.Link onClick={this.props.onJersey} > Jersey </Nav.Link>
+            <Nav.Link onClick={this.props.onPlay} > Players </Nav.Link>
+            <Nav.Link onClick={this.props.onLive} > Live </Nav.Link>
+            <Nav.Link onClick={this.props.onContact} > Contact </Nav.Link>
+            <Nav.Link onClick={this.props.onAdmin} > Admin </Nav.Link>
           </Col>
         </Navbar>
+        
 
       </div>
     );
