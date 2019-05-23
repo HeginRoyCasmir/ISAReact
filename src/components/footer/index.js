25d20d0294b889523cadab2062b524f2f9298827
Hegin Roy C <heginroyc@gmail.com>
2019-05-23 16:20:32 +0530

    Update index.js

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -8,19 +8,19 @@ class Footer extends Component {
     render() {
         return (
             <div className="footer">
-                <Col xs={12} md={12} className="wrapfoot">
-                    <Col xs={2} md={4}>
+                <Col md={12} className="wrapfoot">
+                    <Col className={"col-xs-6"} md={4}>
                         <div className="logos">
                             <Image src={Logo} className="logo" />
                         </div>
                     </Col>
-                    <Col xs={8} md={8}>
+                    <Col md={8}>
                         <Col xs={12} md={12}>
                             <div className="fbshare">
                                 <div className="fb-like" data-href="https://www.facebook.com/ISAKURUMPANAI/" data-width="" data-layout="button_count" data-action="like" data-size="large" data-show-faces="true" data-share="true"></div>
                             </div>
                         </Col>
-                        <Col xs={12} md={12}>
+                        <Col md={12}>
                             <div className="text">
                                 <div className="withSmiles">Made with smile in Kurumpanai, KanyaKumari, TN, INDIA</div>
                             </div>
